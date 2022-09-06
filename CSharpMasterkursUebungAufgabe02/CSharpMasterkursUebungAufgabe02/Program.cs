34bd2de653074e83f57550e8185b480a0619c91d
slackstring <slackstring88@gmail.com>
2022-09-06 20:49:58 +0200

    ErledigtUebungAufgabe02

---
+++ CSharpMasterkursUebungAufgabe02/CSharpMasterkursUebungAufgabe02/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte age;
+            decimal asset;
+            bool marriageStatus;
+            string name;
+            char firstChar;
+            float MuesliPrice;
+
+            writeText("Geben Sie Ihren Vornamen ein:");
+            string preName = Console.ReadLine();
+            writeText("Geben Sie Ihren Nachnahmen ein:");
+            name = Console.ReadLine();
+            writeText("Geben Sie Ihr Alter ein:");
+            age = Convert.ToByte(Console.ReadLine());
+            writeText("Sie heißen " + preName + " " + name + " und sind " + age + " Jahre alt.");
+            Console.ReadKey();
+
+
+        }   
+
+        static void writeText(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
+
