73c1aae51b54896ab794b9dfdd56ca67249fd4b4
slackstring <slackstring88@gmail.com>
2022-09-06 22:05:24 +0200

    Erweiterung Methoden

--- CSharpMasterkursUebungAufgabe02/CSharpMasterkursUebungAufgabe02/Program.cs
+++ CSharpMasterkursUebungAufgabe02/CSharpMasterkursUebungAufgabe02/Program.cs
@@ -26,7 +26,7 @@ namespace Helloworld
             writeText("Geben Sie Ihr Alter ein:");
             age = Convert.ToByte(Console.ReadLine());
             writeText("Sie heißen " + preName + " " + name + " und sind " + age + " Jahre alt.");
-            */
+           
        
             writeText("Zahl 1:");
             double Zahl1 = Convert.ToDouble(Console.ReadLine());
@@ -38,13 +38,29 @@ namespace Helloworld
             Console.WriteLine("{0} * {1} = {2}", Zahl1, Zahl2, Zahl1*Zahl2);
             Console.WriteLine("{0} / {1} = {2}", Zahl1, Zahl2, Zahl1/Zahl2);
             Console.WriteLine("{0} % {1} = {2}", Zahl1, Zahl2, Zahl1%Zahl2);
-
+             
+            string vorname = GetTextStringInput("Vornamen");
+            string nachname = GetTextStringInput("Nachnamen");
+            Console.WriteLine("Hallo " + vorname + " " + nachname);
+            
+            */
+            Console.WriteLine("Radius eingeben:");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Flaeche: {0}", Kreisfläche(radius));
             Console.ReadKey();
 
-
-
         }   
-
+        static double Kreisfläche(double radius)
+        {
+            double pi = Math.PI;
+            return radius * radius * pi;
+        }
+        static string GetTextStringInput(string message)
+        {
+            Console.WriteLine("Gib deinen " + message + " ein"!);
+            
+            return Console.ReadLine();
+        }
         static void writeText(string text)
         {
             Console.WriteLine(text);
